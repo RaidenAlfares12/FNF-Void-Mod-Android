451fa92ff32c57c3fee548c84a87c07879284f43
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-06-10 21:12:24 +0700

    Okay?

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -315,7 +315,7 @@ class TitleState extends MusicBeatState
 
 		if (pressedEnter && !transitioning && skippedIntro)
 		{
-			#if (!switch #if newgrounds) 
+			#if newgrounds
 			NGio.unlockMedal(60960);
 
 			// If it's Friday according to da clock
