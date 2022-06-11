3ee1722302d62976e411dd78b19c73e0baf721ac
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-06-11 10:44:02 +0700

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -13,6 +13,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+import android.FlxVirtualPad;
 #if newgrounds
 import io.newgrounds.NG;
 #end
